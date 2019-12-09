b3734830a678e15c85e71d3fca7d16bf9b208bb0
xiaocode <337317439@qq.com>
2019-12-09 15:20:16 +0800

    xc

--- test-master/src/views/Test/TestVue.vue
+++ test-master/src/views/Test/TestVue.vue
@@ -1,85 +1,33 @@
 <template>
-  <div v-loading="loading">
-    <EditerComponent :catch-data="catchData" :content="formContent"
-                     @blur.native.capture="checkDataUnique"/>
-    <ElButton @click="commit">提交</ElButton>
-    <ElButton>{{count}}</ElButton>
-    <div>{{msg}}</div>
-    <div>{{tmp}}</div>
-    <br/>
-    <span class="red-point">设置</span>
-    <br/>
-    <br/>
+  <div>
     <span>关于</span>
     <RedPointComponent :redval="redCount">
       <template scope='redCount'>
-        <i class="redtip bg-danger" v-show="redCount.text>0">{{redCount.text >= 99 ? '99+' : redCount.text}}</i>
+        <i v-if="redCount.text > 0 && !showRedPoint" class="bg-danger redtip">
+          {{redCount.text >= 99 ? '99+' : redCount.text}}
+        </i>
+        <i v-else class="red-point"></i>
       </template>
     </RedPointComponent>
   </div>
 </template>
 <script>
-  import EditerComponent from './EditerComponent'
   import RedPointComponent from './RedPointComponent'
 
   export default {
     data() {
       return {
-        formContent: '<p>1234567890</p>',
-        tmp: '',
-        count: 0,
-        msg: '',
-        loading: true,
-        redCount: "99",
-      }
-    },
-    mounted() {
-      this.tmp = this.formContent;
-      this.count = this.getSimpleText(this.tmp).length;
-      setTimeout(() => {
-        this.loading = false;
-      }, 1000)
-    },
-    methods: {
-      catchData(value) {
-        console.log(value);
-        this.tmp = value;
-        this.count = this.getSimpleText(this.tmp).length;
-      },
-      //将html标签去除， 统计内容
-      getSimpleText(html) {
-        //匹配html标签的正则表达式，"g"是搜索匹配多个符合的内容
-        let re1 = new RegExp("<.+?>", "g");
-        //执行替换成空字符
-        let msg = html.replace(re1, '');
-        return msg;
-      },
-      checkDataUnique() {
-        if (this.getSimpleText(this.tmp).length <= 0) {
-          this.msg = '要输入内容哦，不能为空';
-        } else if (this.getSimpleText(this.tmp).length > 10) {
-          this.msg = '内容输入的太多了，删除一点吧';
-        } else {
-          this.msg = '内容输入的满足要求， 可以提交';
-        }
-      },
-      commit() {
-        //提交数据
-        this.formContent = this.tmp;
-        console.log(this.formContent);
+        redCount: "10",
+        //是否只显示红点
+        showRedPoint: false
       }
     },
     components: {
-      EditerComponent,
       RedPointComponent
     },
   }
 </script>
 <style lang="scss" rel="stylesheet/scss">
-  #wangeditor {
-    width: 50rem;
-  }
-
   .red-point {
     position: relative;
   }
@@ -95,26 +43,16 @@
   }
 
   .redtip {
-    border-radius: 50%;
-    font-style: normal;
-    color: #fff;
     display: inline-block;
-    width: 1.2em;
-    height: 1.2em;
     text-align: center;
-    line-height: 1.2em;
-    font-size: 0.8em;
-  }
-
-  .redtip-max {
-    border-radius: 50%;
-    font-style: normal;
+    background: #f74c31;
     color: #fff;
-    display: inline-block;
-    width: 1.2em;
-    height: 1.2em;
-    text-align: center;
-    line-height: 1.2em;
-    font-size: 0.8em;
+    font-size: 12px;
+    height: 16px;
+    line-height: 16px;
+    border-radius: 8px;
+    padding: 0 6px;
+    background-clip: padding-box;
+    vertical-align: middle;
   }
 </style>
