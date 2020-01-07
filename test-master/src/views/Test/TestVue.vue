fa8aefa8617973f8cccf3f7544cb1a88367c8e0d
xiaocode <337317439@qq.com>
2020-01-07 17:55:11 +0800

    xc

--- test-master/src/views/Test/TestVue.vue
+++ test-master/src/views/Test/TestVue.vue
@@ -1,58 +1,52 @@
 <template>
-  <div>
-    <span>关于</span>
-    <RedPointComponent :redval="redCount">
-      <template scope='redCount'>
-        <i v-if="redCount.text > 0 && !showRedPoint" class="bg-danger redtip">
-          {{redCount.text >= 99 ? '99+' : redCount.text}}
-        </i>
-        <i v-else class="red-point"></i>
-      </template>
-    </RedPointComponent>
-  </div>
+  <el-form :model="monitorForm" :rules="monitorForm.monitorRules" ref="monitorForm">
+    <el-table
+      :data="monitorForm.monitorData"
+      style="width: 100%">
+      <el-table-column
+        label="日期"
+        width="180">
+        <template slot-scope="scope">
+          <el-form-item
+            :prop="'monitorData.' + scope.$index + '.date'"
+            :rules="monitorForm.monitorRules.date">
+            <el-input
+              class="light-blue-theme"
+              clearable
+              v-model="scope.row.date"
+              placeholder="请输入date"/>
+          </el-form-item>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-form>
 </template>
-<script>
-  import RedPointComponent from './RedPointComponent'
 
+<script>
   export default {
     data() {
       return {
-        redCount: "10",
-        //是否只显示红点
-        showRedPoint: false
+        monitorForm: {
+          monitorData: [{
+            date: '2016-05-02',
+          }, {
+            date: '2016-05-04',
+          }, {
+            date: '2016-05-01',
+          }],
+          monitorRules: {
+            date: [
+              { required: true, message: 'date不能为空', trigger: 'blur' }
+            ],
+            // type: [
+            //   { required: true, message: '品牌名称不能为空', trigger: 'blur' }
+            // ],
+            // height: [
+            //   { required: true, message: '高度不能为空', trigger: 'blur' }
+            // ]
+          }
+        }
       }
-    },
-    components: {
-      RedPointComponent
     },
   }
 </script>
-<style lang="scss" rel="stylesheet/scss">
-  .red-point {
-    position: relative;
-  }
-
-  .red-point::before {
-    content: " ";
-    border: 3px solid red; /*设置红色*/
-    border-radius: 3px; /*设置圆角*/
-    position: absolute;
-    z-index: 1000;
-    right: 0;
-    margin-right: -8px;
-  }
-
-  .redtip {
-    display: inline-block;
-    text-align: center;
-    background: #f74c31;
-    color: #fff;
-    font-size: 12px;
-    height: 16px;
-    line-height: 16px;
-    border-radius: 8px;
-    padding: 0 6px;
-    background-clip: padding-box;
-    vertical-align: middle;
-  }
-</style>
