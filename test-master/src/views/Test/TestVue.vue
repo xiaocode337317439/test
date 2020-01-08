01836594516682aa6d85d063d60d76b5f473a4e4
xiaocode <337317439@qq.com>
2020-01-08 14:32:57 +0800

    xc

--- test-master/src/views/Test/TestVue.vue
+++ test-master/src/views/Test/TestVue.vue
@@ -8,15 +8,21 @@
         width="180">
         <template slot-scope="scope">
           <el-form-item
+            style="height: 100%"
             :prop="'monitorData.' + scope.$index + '.date'"
             :rules="monitorForm.monitorRules.date">
             <el-input
-              class="light-blue-theme"
+              style="height: 100%"
               clearable
               v-model="scope.row.date"
               placeholder="请输入date"/>
           </el-form-item>
         </template>
+      </el-table-column>
+      <el-table-column
+        label="日期"
+        prop="date"
+        width="180">
       </el-table-column>
     </el-table>
   </el-form>
@@ -25,28 +31,58 @@
 <script>
   export default {
     data() {
+      let validateValue = (rule, value, callback) => {
+        //map
+        this.callbackObj[rule.fullField] = callback;
+        let num = 0;
+        this.monitorForm.monitorData.forEach(item => {
+          num += parseInt(item);
+        });
+        console.log(num);
+        if (num !== 100) {
+          Object.keys(this.callbackObj).forEach(key => {
+            this.callbackObj[key](new Error('总和不等于100'));
+          })
+          // callback(new Error('总和不等于100'));
+        } else {
+          Object.keys(this.callbackObj).forEach(key => {
+            this.callbackObj[key]();
+          })
+          // callback();
+        }
+      };
       return {
+        callbackObj: {},
         monitorForm: {
           monitorData: [{
-            date: '2016-05-02',
+            date: '1',
           }, {
-            date: '2016-05-04',
+            date: '2',
           }, {
-            date: '2016-05-01',
+            date: '3',
           }],
           monitorRules: {
             date: [
-              { required: true, message: 'date不能为空', trigger: 'blur' }
+              {required: true, trigger: 'blur', validate: validateValue}
             ],
-            // type: [
-            //   { required: true, message: '品牌名称不能为空', trigger: 'blur' }
-            // ],
-            // height: [
-            //   { required: true, message: '高度不能为空', trigger: 'blur' }
-            // ]
           }
         }
       }
     },
   }
 </script>
+<style scoped>
+  .el-form-item {
+    margin-bottom: 0;
+  }
+
+  /deep/.el-form-item__error {
+    color: #ED4014;
+    font-size: 12px;
+    line-height: 1;
+    padding-top: 4px;
+    position: relative;
+    top: 100%;
+    left: 0;
+  }
+</style>
