bff07c1d08432653e2ed26c953abc895258f07e5
xiaocode337317439 <337317439@qq.com>
2020-01-08 22:55:58 +0800

    Update TestVue.vue

--- test-master/src/views/Test/TestVue.vue
+++ test-master/src/views/Test/TestVue.vue
@@ -1,88 +1,143 @@
 <template>
-  <el-form :model="monitorForm" :rules="monitorForm.monitorRules" ref="monitorForm">
-    <el-table
-      :data="monitorForm.monitorData"
-      style="width: 100%">
-      <el-table-column
-        label="日期"
-        width="180">
-        <template slot-scope="scope">
-          <el-form-item
-            style="height: 100%"
-            :prop="'monitorData.' + scope.$index + '.date'"
-            :rules="monitorForm.monitorRules.date">
-            <el-input
-              style="height: 100%"
-              clearable
-              v-model="scope.row.date"
-              placeholder="请输入date"/>
-          </el-form-item>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="日期"
-        prop="date"
-        width="180">
-      </el-table-column>
-    </el-table>
+  <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="100px" class="demo-dynamic">
+    <el-form-item
+      prop="email"
+      label="邮箱"
+      :rules="[
+      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+    ]"
+    >
+      <el-input v-model="dynamicValidateForm.email"></el-input>
+    </el-form-item>
+    <el-form-item
+      v-for="(domain, index) in dynamicValidateForm.domains"
+      :label="'域名' + index"
+      :key="domain.key"
+      :prop="'domains.' + index + '.value'"
+      :rules="flag?rules1.aa:rules2.aa"
+    >
+      <!--<el-input v-model="domain.value"></el-input>-->
+      <el-select
+        v-model="domain.value"
+        multiple
+        filterable
+        remote
+        reserve-keyword
+        @focus="focusA"
+        placeholder="请输入关键词"
+        :remote-method="remoteMethod"
+        :loading="loading">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <el-button @click.prevent="removeDomain(domain)">删除</el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('dynamicValidateForm')">提交</el-button>
+      <el-button @click="addDomain">新增域名</el-button>
+      <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
+    </el-form-item>
   </el-form>
 </template>
 
 <script>
   export default {
     data() {
-      let validateValue = (rule, value, callback) => {
-        //map
-        this.callbackObj[rule.fullField] = callback;
-        let num = 0;
-        this.monitorForm.monitorData.forEach(item => {
-          num += parseInt(item);
-        });
-        console.log(num);
-        if (num !== 100) {
-          Object.keys(this.callbackObj).forEach(key => {
-            this.callbackObj[key](new Error('总和不等于100'));
-          })
-          // callback(new Error('总和不等于100'));
-        } else {
-          Object.keys(this.callbackObj).forEach(key => {
-            this.callbackObj[key]();
-          })
-          // callback();
-        }
-      };
       return {
-        callbackObj: {},
-        monitorForm: {
-          monitorData: [{
-            date: '1',
-          }, {
-            date: '2',
-          }, {
-            date: '3',
+        flag: true,
+        rules1: {
+          aa: {required: true, message: '域名不能为空111', trigger: 'blur'}
+        },
+        dynamicValidateForm: {
+          domains: [{
+            value: ''
           }],
-          monitorRules: {
-            date: [
-              {required: true, trigger: 'blur', validate: validateValue}
-            ],
-          }
+          email: ''
+        },
+        options: [],
+        list: [],
+        loading: false,
+        states: ["Alabama", "Alaska", "Arizona",
+          "Arkansas", "California", "Colorado",
+          "Connecticut", "Delaware", "Florida",
+          "Georgia", "Hawaii", "Idaho", "Illinois",
+          "Indiana", "Iowa", "Kansas", "Kentucky",
+          "Louisiana", "Maine", "Maryland",
+          "Massachusetts", "Michigan", "Minnesota",
+          "Mississippi", "Missouri", "Montana",
+          "Nebraska", "Nevada", "New Hampshire",
+          "New Jersey", "New Mexico", "New York",
+          "North Carolina", "North Dakota", "Ohio",
+          "Oklahoma", "Oregon", "Pennsylvania",
+          "Rhode Island", "South Carolina",
+          "South Dakota", "Tennessee", "Texas",
+          "Utah", "Vermont", "Virginia",
+          "Washington", "West Virginia", "Wisconsin",
+          "Wyoming"]
+      };
+    },
+    mounted() {
+      this.list = this.states.map(item => {
+        return {value: `value:${item}`, label: `label:${item}`};
+      });
+    },
+    computed: {
+      rules2() {
+        return {
+          ...this.rules1,
+          aa: {required: true, message: '域名不能为空111', trigger: ['blur', 'change']}
         }
       }
     },
+    methods: {
+      focusA() {
+        this.flag = false;
+      },
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      removeDomain(item) {
+        var index = this.dynamicValidateForm.domains.indexOf(item)
+        if (index !== -1) {
+          this.dynamicValidateForm.domains.splice(index, 1)
+        }
+      },
+      addDomain() {
+        this.flag = true;
+        this.dynamicValidateForm.domains.push({
+          value: '',
+          key: Date.now()
+        });
+      },
+      remoteMethod(query) {
+        if (query !== '') {
+          this.loading = true;
+          setTimeout(() => {
+            this.loading = false;
+            this.options = this.list.filter(item => {
+              return item.label.toLowerCase()
+                .indexOf(query.toLowerCase()) > -1;
+            });
+          }, 200);
+        } else {
+          this.options = [];
+        }
+      }
+    }
   }
 </script>
-<style scoped>
-  .el-form-item {
-    margin-bottom: 0;
-  }
-
-  /deep/.el-form-item__error {
-    color: #ED4014;
-    font-size: 12px;
-    line-height: 1;
-    padding-top: 4px;
-    position: relative;
-    top: 100%;
-    left: 0;
-  }
-</style>
