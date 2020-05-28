5240ab119cdd3105babbe6a7511d4c07cde23ffd
xiaocode <337317439@qq.com>
2020-05-28 17:58:41 +0800

    xc

--- test-master/src/views/Test/TestVue.vue
+++ test-master/src/views/Test/TestVue.vue
@@ -1,70 +1,107 @@
 <template>
-  <el-select
-    v-model="value"
-    multiple
-    filterable
-    remote
-    reserve-keyword
-    placeholder="请输入关键词"
-    :remote-method="remoteMethod"
-    :loading="loading">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.value"
-      :value="item.value">
-    </el-option>
-  </el-select>
+  <el-table
+    :data="tableData"
+    :span-method="objectSpanMethod"
+    border
+    style="width: 100%; margin-top: 20px">
+    <el-table-column
+      prop="id"
+      label="ID"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名">
+    </el-table-column>
+    <el-table-column
+      prop="amount1"
+      label="数值 1（元）">
+    </el-table-column>
+    <el-table-column
+      prop="amount2"
+      label="数值 2（元）">
+    </el-table-column>
+    <el-table-column
+      prop="amount3"
+      label="数值 3（元）">
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        options: [],
-        value: ['Alabama(这里是工号0)', 'Colorado(这里是工号5)', 'Delaware(这里是工号7)'],
-        list: [],
-        loading: false,
-      }
+        tableData: [{
+          id: '12987122',
+          name: '王小虎',
+          amount1: '234',
+          amount2: '3.2',
+          amount3: 10
+        }, {
+          id: '12987122',
+          name: '王小虎',
+          amount1: '165',
+          amount2: '4.43',
+          amount3: 12
+        }, {
+          id: '12987124',
+          name: '王小虎',
+          amount1: '324',
+          amount2: '1.9',
+          amount3: 9
+        }, {
+          id: '12987125',
+          name: '王小虎',
+          amount1: '621',
+          amount2: '2.2',
+          amount3: 17
+        }, {
+          id: '12987125',
+          name: '王小虎',
+          amount1: '539',
+          amount2: '4.1',
+          amount3: 15
+        }]
+      };
     },
     mounted() {
-      let states = ["Alabama", "Alaska", "Arizona",
-        "Arkansas", "California", "Colorado",
-        "Connecticut", "Delaware", "Florida",
-        "Georgia", "Hawaii", "Idaho", "Illinois",
-        "Indiana", "Iowa", "Kansas", "Kentucky",
-        "Louisiana", "Maine", "Maryland",
-        "Massachusetts", "Michigan", "Minnesota",
-        "Mississippi", "Missouri", "Montana",
-        "Nebraska", "Nevada", "New Hampshire",
-        "New Jersey", "New Mexico", "New York",
-        "North Carolina", "North Dakota", "Ohio",
-        "Oklahoma", "Oregon", "Pennsylvania",
-        "Rhode Island", "South Carolina",
-        "South Dakota", "Tennessee", "Texas",
-        "Utah", "Vermont", "Virginia",
-        "Washington", "West Virginia", "Wisconsin",
-        "Wyoming"];
-      //封装数据源, 假如id就是 工号
-      this.list = states.map((item, index) => {
-        return {id: index, value: `${item}(这里是工号${index})`};
-      });
+      //在数据加载成功后调用这一句
+      this.rowSpanArr = this.getRowSpan('id');
     },
     methods: {
-      remoteMethod(query) {
-        if (query !== '') {
-          this.loading = true;
-          setTimeout(() => {
-            this.loading = false;
-            this.options = this.list.filter(item => {
-              return item.value.toLowerCase()
-                .indexOf(query.toLowerCase()) > -1;
-            });
-          }, 200);
-        } else {
-          this.options = [];
+      getRowSpan(key) {
+        // 获取合并的数组
+        const spanArr = [];
+        let position = 0;
+        this.tableData.forEach((item, index) => {
+          if (index === 0) {
+            spanArr.push(1);
+            position = 0;
+          } else {
+            //合并指定的属性key
+            if (this.tableData[index][key] === this.tableData[index - 1][key]) {
+              spanArr[position] += 1;
+              spanArr.push(0);
+              this.tableData[index][key] = this.tableData[index - 1][key];
+            } else {
+              spanArr.push(1);
+              position = index;
+            }
+          }
+        });
+        return spanArr;
+      },
+      objectSpanMethod({row, column, rowIndex, columnIndex}) {
+        // 只合并区域位置
+        if (columnIndex === 0) {
+          const row = this.rowSpanArr[rowIndex];
+          return {
+            rowspan: row, //行
+            colspan: 1 //列
+          };
         }
       }
     }
-  }
+  };
 </script>
---
+++ test-master/src/views/Test/TestVue4.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-select
+    v-model="value"
+    multiple
+    filterable
+    remote
+    reserve-keyword
+    placeholder="请输入关键词"
+    :remote-method="remoteMethod"
+    :loading="loading">
+    <el-option
+      v-for="item in options"
+      :key="item.id"
+      :label="item.value"
+      :value="item.value">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        options: [],
+        value: ['Alabama(这里是工号0)', 'Colorado(这里是工号5)', 'Delaware(这里是工号7)'],
+        list: [],
+        loading: false,
+      }
+    },
+    mounted() {
+      let states = ["Alabama", "Alaska", "Arizona",
+        "Arkansas", "California", "Colorado",
+        "Connecticut", "Delaware", "Florida",
+        "Georgia", "Hawaii", "Idaho", "Illinois",
+        "Indiana", "Iowa", "Kansas", "Kentucky",
+        "Louisiana", "Maine", "Maryland",
+        "Massachusetts", "Michigan", "Minnesota",
+        "Mississippi", "Missouri", "Montana",
+        "Nebraska", "Nevada", "New Hampshire",
+        "New Jersey", "New Mexico", "New York",
+        "North Carolina", "North Dakota", "Ohio",
+        "Oklahoma", "Oregon", "Pennsylvania",
+        "Rhode Island", "South Carolina",
+        "South Dakota", "Tennessee", "Texas",
+        "Utah", "Vermont", "Virginia",
+        "Washington", "West Virginia", "Wisconsin",
+        "Wyoming"];
+      //封装数据源, 假如id就是 工号
+      this.list = states.map((item, index) => {
+        return {id: index, value: `${item}(这里是工号${index})`};
+      });
+    },
+    methods: {
+      remoteMethod(query) {
+        if (query !== '') {
+          this.loading = true;
+          setTimeout(() => {
+            this.loading = false;
+            this.options = this.list.filter(item => {
+              return item.value.toLowerCase()
+                .indexOf(query.toLowerCase()) > -1;
+            });
+          }, 200);
+        } else {
+          this.options = [];
+        }
+      }
+    }
+  }
+</script>
